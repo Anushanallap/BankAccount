41dda74c68e42465cf5dfc0699bdc01c4f460014
Anushanallap <anusha.n468@gmail.com>
2021-12-20 12:05:10 +0530

    Add super n sub class

---
+++ Account.java
@@ -0,0 +1,22 @@
+package Accountbalance;
+
+public class Account {
+	
+	    private double acc_bal;
+	    
+	    public double getAcc_bal(){
+	        return acc_bal;
+	    }
+	    public void setAcc_bal(double acc_bal){
+	        this.acc_bal = acc_bal;
+	    }
+		public void credit(int i) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void debit(int i) {
+			// TODO Auto-generated method stub
+			
+		}
+
+}
